0eab9caf4de2bfc6eec4ec77652d6bee63183f13
iamsk <iamsk.info@gmail.com>
2022-03-12 22:24:37 +0800

    for all

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="text-white">
     <div class="text-center mt-12 max-w-3xl mx-auto">
       <h1 class="font-semibold text-4xl">NFT Search</h1>
-      <p class="text-lg mt-1">Quickly upload image and locate on NFT market, supports <a href="https://www.azuki.com/" target="_blank"><strong>Azuki</strong></a> currently!</p>
+      <p class="text-lg mt-1">Quickly upload image and locate on NFT market, support <strong>ALL NFTs</strong>, but more accurate for <a href="https://boredapeyachtclub.com/" target="_blank"><strong>BAYC</strong></a> and <a href="https://www.azuki.com/" target="_blank"><strong>Azuki</strong></a> !</p>
       <Uploader ref="uploader" />
     </div>
     <BackgroundEffects />
